4c8d9c51a64e1ac4520f11fb6d1469587890086b
ziedzayani <zayani.zied@gmail.com>
2023-02-10 15:40:06 +0100

    Update validator-utils.ts

--- src/app/error-message/validator-utils.ts
+++ src/app/error-message/validator-utils.ts
@@ -1,13 +1,15 @@
-export const getValidatorErrorMessage = (validatorName: string, validatorErrorValue?: any): string|undefined => {
+import { ValidationErrors } from "@angular/forms";
 
-    let args = messages.get(validatorName)?.validatorErrorsKey?.map(name => validatorErrorValue[name]);
+export const getValidatorErrorMessage = (validatorName: string, validatorErrorValue?: ValidationErrors): string|undefined => {
+
+    let args = messages.get(validatorName)?.validatorErrorsKey?.map(name => validatorErrorValue?.[name]);
     return (args) ? stringFormat(messages.get(validatorName)?.message,...args) : messages.get(validatorName)?.message;
 }
 
 const  messages = new Map<string, {message : string,validatorErrorsKey? : string[]}>([
     ['required',  { message : 'This field is required'} ],
     ['minlength', { message : 'Password must be at least {0} characters long' ,   validatorErrorsKey :['requiredLength']}],
-    ['maxlength', { message : 'Password cannot be more than {0} characters {1} long', validatorErrorsKey :['requiredLength']}],
+    ['maxlength', { message : 'Password cannot be more than {0} characters long', validatorErrorsKey :['requiredLength']}],
     ['email',     { message : 'Email must be a valid email address'}],
 ]);
 
